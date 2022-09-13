42cb99889a8d68e66f938d8efc7ee36639d0e092
acferlucas <lafs@discente.ifpe.edu.br>
2022-09-13 19:23:02 -0300

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/ads', (req, res) => {
+  return res.json([
+    { id: 1, name: 'Anuncio 1' },
+    { id: 2, name: 'Anuncio 1' },
+    { id: 3, name: 'Anuncio 1' },
+    { id: 4, name: 'Anuncio 1' },
+  ]);
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+})
